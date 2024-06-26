7084478567056ef3f4be06acaa9fddb14d0e2a83
celestinogabrie <27865754-celestinogabrie@users.noreply.replit.com>
2024-06-26 22:36:52 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@First-Code/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(void) {
+  int n1,n2,media;
+  scanf("%d",&n1);
+  scanf("%d",&n2);
+  media = (n1 + n2) / 2;
+  printf("%d",media);
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+  return 0;
+}
